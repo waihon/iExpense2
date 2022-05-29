55ec574f485586e9b57c5581321df46b92a38380
Waihon Yew <yewwaihon@gmail.com>
2022-05-29 16:19:30 +0800

    Use @State for struct object

--- iExpense2/ContentView.swift
+++ iExpense2/ContentView.swift
@@ -7,10 +7,22 @@
 
 import SwiftUI
 
+struct User {
+    var firstName = "Bilbo"
+    var lastName = "Buggins"
+}
+
 struct ContentView: View {
+    @State private var user = User()
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Your name is \(user.firstName) \(user.lastName)")
+
+            TextField("First name", text: $user.firstName)
+            TextField("Last name", text: $user.lastName)
+        }
+        .padding()
     }
 }
 
