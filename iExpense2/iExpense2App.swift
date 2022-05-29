62e0237dedac6029550391c5118ea9f9bdc37101
Waihon Yew <yewwaihon@gmail.com>
2022-05-29 14:42:08 +0800

    Initial Commit

---
+++ iExpense2/iExpense2App.swift
@@ -0,0 +1,17 @@
+//
+//  iExpense2App.swift
+//  iExpense2
+//
+//  Created by Waihon Yew on 29/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpense2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
