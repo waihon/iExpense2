5438d67a63a9a3087469f03ef6d80b09be78c270
Waihon Yew <yewwaihon@gmail.com>
2022-05-29 16:49:34 +0800

    Replace @State with @StateObject, and make the class observes ObservableObject and its properties @Published

--- iExpense2/ContentView.swift
+++ iExpense2/ContentView.swift
@@ -7,13 +7,22 @@
 
 import SwiftUI
 
-class User {
-    var firstName = "Bilbo"
-    var lastName = "Buggins"
+class User: ObservableObject {
+    // @ObservableObject protocol has no requirements, and really all it
+    // means is "we want other things to be able to monitor this for changes."
+
+    // @Published is more or less half of @State: it tells Swift that
+    // whenever tagged properties changes, it should send an announcement
+    // out to any SwiftUI views that are watching that they shoud reload.
+    @Published var firstName = "Bilbo"
+    @Published var lastName = "Buggins"
 }
 
 struct ContentView: View {
-    @State private var user = User()
+    // @StateObject is the other half of @State - it tells SwiftUI that we're
+    // creating a new class instance that should be watched for any change
+    // announcements.
+    @StateObject var user = User()
 
     var body: some View {
         VStack {
