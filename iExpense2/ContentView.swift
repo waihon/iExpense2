69e8673a4e4e58ebd511dc03139dba8c790f1ccb
Waihon Yew <yewwaihon@gmail.com>
2022-06-01 20:36:09 +0800

    Dismiss the second view

--- iExpense2/ContentView.swift
+++ iExpense2/ContentView.swift
@@ -8,10 +8,14 @@
 import SwiftUI
 
 struct SecondView: View {
+    @Environment(\.dismiss) var dismiss
+
     let name: String
 
     var body: some View {
-        Text("Hello, \(name)")
+        Button("Dismiss") {
+            dismiss()
+        }
     }
 }
 
