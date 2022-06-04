a0060426a45fde2b1bedcd5254606e941f4df174
Waihon Yew <yewwaihon@gmail.com>
2022-06-04 11:15:21 +0800

    Represent what an expense is using a struct

---
+++ iExpense2/ExpenseItem.swift
@@ -0,0 +1,14 @@
+//
+//  ExpenseItem.swift
+//  iExpense2
+//
+//  Created by Waihon Yew on 04/06/2022.
+//
+
+import Foundation
+
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Double
+}
