8ea3e66ae1c3b5f3cdb93d69374567e294858fda
Waihon Yew <yewwaihon@gmail.com>
2022-06-04 15:48:38 +0800

    Save an expense item by appending it to expenses

--- AddView.swift
+++ AddView.swift
@@ -32,6 +32,12 @@ struct AddView: View {
                     .keyboardType(.decimalPad)
             }
             .navigationTitle("Add new expense")
+            .toolbar {
+                Button("Save") {
+                    let item = ExpenseItem(name: name, type: type, amount: amount)
+                    expenses.items.append(item)
+                }
+            }
             .onAppear() {
                 DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
                     isFocused = true
